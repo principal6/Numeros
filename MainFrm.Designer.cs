4537e761cda0e8692a4c8f30bee9cba46682778b
Jesus Kim <42127220+principal6@users.noreply.github.com>
2019-09-28 20:18:42 +0900

    Initial commit

---
+++ MainFrm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Numeros
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(264, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(264, 31);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(264, 31);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(264, 31);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(21, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(264, 31);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(21, 302);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(264, 31);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 31);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(304, 343);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainFrm";
+            this.Text = "스페인어 숫자 연습기";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFrm_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MainFrm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros
+{
+    public partial class MainFrm : Form
+    {
+        private Random m_Rnd = new Random(System.DateTime.Now.Second);
+
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Text = "랜덤 숫자 생성을 시작하려면";
+            label3.Text = "아무 키나 누르십시오";
+            label1.Text = label4.Text = label5.Text = label6.Text = label7.Text = label8.Text = label9.Text = "";
+        }
+
+        private void MainFrm_KeyDown(object sender, KeyEventArgs e)
+        {
+            label2.TextAlign = ContentAlignment.MiddleRight;
+            label3.TextAlign = ContentAlignment.MiddleRight;
+
+            string p1 = m_Rnd.Next(1000).ToString();
+            label1.Text = p1.Substring(0, p1.Length - 1) + "," +
+                p1.Substring(p1.Length - 1) + "%";
+            if (p1.Length == 1) label1.Text = "0" + label1.Text;
+
+            string p2 = m_Rnd.Next(10, 10000).ToString();
+            label2.Text = p2.Substring(0, p2.Length - 2) + "," +
+                p2.Substring(p2.Length - 2) + "%";
+            if (p2.Length == 2) label2.Text = "0" + label2.Text;
+
+            string n1 = m_Rnd.Next(10, 99).ToString();
+            label3.Text = n1.Substring(0, 1) + "." + n1.Substring(1) + "00 personas";
+
+            string n2 = m_Rnd.Next(100, 999).ToString();
+            label4.Text = n2.Substring(0, 2) + "." + n2.Substring(2) + "00 personas";
+
+            string n3 = m_Rnd.Next(1000, 9999).ToString();
+            label5.Text = n3.Substring(0, 3) + "." + n3.Substring(3) + "00 personas";
+
+            string n4 = m_Rnd.Next(100, 999).ToString();
+            label6.Text = "$ " + n4.Substring(0, 1) + "." + n4.Substring(1, 2) + "0.000";
+
+            string n5 = m_Rnd.Next(100, 999).ToString();
+            label7.Text = "$ " + n5.Substring(0, 2) + "." + n5.Substring(2) + "00.000";
+
+            string n6 = m_Rnd.Next(100, 999).ToString();
+            label8.Text = "$ " + n6 + ".000.000";
+
+            string n7 = m_Rnd.Next(100, 999).ToString();
+            label9.Text = "$ " + n7.Substring(0, 1) + "." + n7.Substring(1, 2) + "0.000.000";
+        }
+    }
+}
